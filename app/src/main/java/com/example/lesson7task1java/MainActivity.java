91a80bd1eb4a88fa268d0d51052eb9c34b1c8245
temur007222 <temurxaydarov1@gmail.com>
2022-04-17 06:15:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lesson7task1java/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.lesson7task1java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputUsername;
+    private TextInputLayout textInputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textInputEmail = findViewById(R.id.text_input_email);
+        textInputUsername = findViewById(R.id.text_input_username);
+        textInputPassword = findViewById(R.id.text_input_password);
+    }
+
+    private boolean validateEmail(){
+        String emailInput = textInputEmail.getEditText().getText().toString().trim();
+
+        if (emailInput.isEmpty()){
+            textInputEmail.setError("Field can't be empty");
+            return false;
+        }else {
+            textInputEmail.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateUsername(){
+        String usernameInput = textInputEmail.getEditText().getText().toString().trim();
+
+        if (usernameInput.isEmpty()){
+            textInputEmail.setError("Field can't be empty");
+            return false;
+        }else if(usernameInput.length() > 15) {
+            textInputUsername.setError("Username is too long");
+            return false;
+        }else {
+            textInputUsername.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword(){
+        String passwordInput = textInputEmail.getEditText().getText().toString().trim();
+
+        if (passwordInput.isEmpty()){
+            textInputPassword.setError("Field can't be empty");
+            return false;
+        }else {
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void confirmInput(View view){
+        if (!validateEmail() | !validateUsername() | !validatePassword()){
+            return;
+        }
+        String input = "Email: " + textInputEmail.getEditText().getText().toString();
+        input += "\n";
+        input += "Username: " + textInputUsername.getEditText().getText().toString();
+        input += "\n";
+        input += "Password: " + textInputPassword.getEditText().getText().toString();
+
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+    }
+}
